2eaca29c857be5742d7a12000ccfa9d987118116
Saptarshi Roy Choudhuri <saptarshi.roychoudhuri@gmail.com>
2016-12-31 12:27:38 +0530

    Modifications committed

--- app/src/main/java/saptarshi/com/task6/MainActivity.java
+++ app/src/main/java/saptarshi/com/task6/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,11 +15,16 @@ public class MainActivity extends AppCompatActivity {
         FragmentTransaction frag = getSupportFragmentManager().beginTransaction();
         frag.replace(R.id.container, new FirstFragment());
         frag.commit();
-    }
+     }
+
+
     public void replaceFragment (View v){
         FragmentTransaction frag = getSupportFragmentManager().beginTransaction();
         frag.replace(R.id.container, new SecondFragment());
         frag.commit();
+        Button but = (Button) findViewById(R.id.button_1);
+        but.setVisibility(View.GONE);
+
 
     }
 }
